2c9d8d2c7bf76659248f7a8d946b6fc71b159c52
Dorian Calin <dorin.m.calin@gmail.com>
2020-03-31 12:07:48 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,11 @@
-def find_min_value(array)
-  # Add your solution here
+def find_min_value(nums)
+ minimum = nums[0]
+nums.each do |element|
+if element < minimum
+minimum = element
 end
+end
+return minimum
+end
+
+
