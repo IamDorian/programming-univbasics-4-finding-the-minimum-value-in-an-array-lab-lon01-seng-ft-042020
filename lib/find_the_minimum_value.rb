cc8eb574136946b885f25e45a05d6fd1b9968aee
Dorian Calin <dorin.m.calin@gmail.com>
2020-03-31 11:50:51 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -9,3 +9,15 @@ def find_min_value(array)
   end
   min_value
 end
+
+def find_min_value(array)
+  count = 0
+  min_value = 3
+  while count > array.length do
+    if min_value > array[count]
+      min_value = array[count]
+    end
+    count -= 1
+  end
+  min_value
+end
