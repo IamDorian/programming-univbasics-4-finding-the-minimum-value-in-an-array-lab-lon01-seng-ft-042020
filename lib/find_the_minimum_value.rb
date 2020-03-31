70c41ccd00245b082001c5782ae72be4e3040ab4
Dorian Calin <dorin.m.calin@gmail.com>
2020-03-31 12:00:32 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,13 +1,5 @@
 def find_min_value(nums)
-  max = nums[0];
-  min = nums[0];
-  nums.each do |item|
-    if(item > max)
-			max = item;
-		elsif(item < min)
-			min = item
-    end 
-   end    
-  return (max-min)
+  (1…n).map {a=l.min ; l=l-[a]; a } 
+  end
 end
 
